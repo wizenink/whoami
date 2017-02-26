0505c3ceee0a5d1846634591e4c71204d89cb658
David Maseda <david.maseda@hotmail.com>
2017-02-26 12:07:11 +0000

    Test build

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
+app.get('/',function(req,res){
+    var response = {
+        "ipaddress":req.ip
+        ,"language":req.get("Accept-Language")
+        ,"software":req.get("User-Agent").match(/\((.+)\)/)[1]
+    }
+    res.send(response);
+});
 app.listen(port,function(){
     console.log('App listening on port ' + port);
 });
